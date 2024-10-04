81413d863a2ec4cdeee175b9e3f4d4c3bff79e44
Fabian_Martinez <f1a7b95@gmail.com>
2024-10-03 21:26:19 -0500

    [NUEVO] Calculadora simple

---
+++ Scripts/ejercicios.js
@@ -0,0 +1,28 @@
+
+function sumar() {
+	let num1 = parseInt(document.getElementById("num1").value);
+	let num2 = parseInt(document.getElementById("num2").value);
+	let resultado = num1 + num2;
+	document.getElementById("resultado").value = resultado;
+}
+
+function restar() {
+	let num1 = parseInt(document.getElementById("num1").value);
+	let num2 = parseInt(document.getElementById("num2").value);
+	let resultado = num1 - num2;
+	document.getElementById("resultado").value = resultado;
+}
+
+function multiplicar() {
+	let num1 = parseInt(document.getElementById("num1").value);
+	let num2 = parseInt(document.getElementById("num2").value);
+	let resultado = num1 * num2;
+	document.getElementById("resultado").value = resultado;
+}
+
+function dividir() {
+	let num1 = parseInt(document.getElementById("num1").value);
+	let num2 = parseInt(document.getElementById("num2").value);
+	let resultado = num1 / num2;
+	document.getElementById("resultado").value = resultado;
+}
