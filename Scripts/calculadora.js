93fce947a878da04b0b26f896aed15f6f56ef429
Fabian_Martinez <f1a7b95@gmail.com>
2024-10-03 21:50:11 -0500

    [Actualización] Adición README con modificaciones

--- Scripts/calculadora.js
+++ Scripts/calculadora.js
@@ -1,28 +1,30 @@
 
+// Esta es la función de sumar solamente 
 function sumar() {
 	let num1 = parseInt(document.getElementById("num1").value);
 	let num2 = parseInt(document.getElementById("num2").value);
 	let resultado = num1 + num2;
-	document.getElementById("resultado").value = resultado;
+	document.getElementById("Resultado").value = resultado;
 }
 
+// Esta es la función de restar solamente
 function restar() {
 	let num1 = parseInt(document.getElementById("num1").value);
 	let num2 = parseInt(document.getElementById("num2").value);
 	let resultado = num1 - num2;
-	document.getElementById("resultado").value = resultado;
+	document.getElementById("Resultado").value = resultado;
 }
-
+// Esta es la función de multiplicar solamente
 function multiplicar() {
 	let num1 = parseInt(document.getElementById("num1").value);
 	let num2 = parseInt(document.getElementById("num2").value);
 	let resultado = num1 * num2;
-	document.getElementById("resultado").value = resultado;
+	document.getElementById("Resultado").value = resultado;
 }
-
+// Esta es la función de dividir solamente
 function dividir() {
 	let num1 = parseInt(document.getElementById("num1").value);
 	let num2 = parseInt(document.getElementById("num2").value);
 	let resultado = num1 / num2;
-	document.getElementById("resultado").value = resultado;
+	document.getElementById("Resultado").value = resultado;
 }
